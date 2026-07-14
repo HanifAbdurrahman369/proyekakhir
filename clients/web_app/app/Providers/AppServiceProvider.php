1122414631d64e33bdcad84fb720011fa7bb6b81
HanifAbdurrahman369 <ahsanadamabd@gmail.com>
2026-07-14 08:45:57 +0800

    fixxxxxxx

--- clients/web_app/app/Providers/AppServiceProvider.php
+++ clients/web_app/app/Providers/AppServiceProvider.php
@@ -19,6 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (env('FORCE_HTTPS', true)) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         \Illuminate\Support\Facades\Http::globalOptions([
             'headers' => [
                 'Connection' => 'close',
